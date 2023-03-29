fcbab060e92332b499f2e994547438a6f0df69b9
Guillaume-Linez <guillaumelinez1@gmail.com>
2023-03-29 14:37:41 +0200

    creation page jeu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+
+import 'grille_du_jeu.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,44 +14,68 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(title: 'Masyu'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
+
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: const Color(0xFFAE0000),
-      body: SizedBox(
-        width: 200,
-        height: 45,
-        child: TextButton(
-          style: TextButton.styleFrom(
-            backgroundColor: Color(),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-          ),
-          onPressed: () {},
-          child: const Text(
-            "TextButton",
-            style: TextStyle(
-              color: Color(0xffffffff),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ),
+          ],
         ),
       ),
+      //create a button to navigate to the grille_du_jeu.dart
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) =>  GrilleDuJeu()),
+          );
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.radio),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
