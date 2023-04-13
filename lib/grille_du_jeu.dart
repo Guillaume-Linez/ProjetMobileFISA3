1bfb6cdc9d5d05cf3b7a5efcfb6e1c64ba5d2736
aladinMJ <alaaeddin.mj@gmail.com>
2023-04-13 14:39:16 +0200

    test_aladin

--- lib/grille_du_jeu.dart
+++ lib/grille_du_jeu.dart
@@ -7,17 +7,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-// String createOrderMessage() {
-//   var order = fetchUserOrder();
-//   return 'Your order is: $order';
-// }
 
-// Future<String> fetchUserOrder() =>
-//     // Imagine that this function is more complex and slow.
-//     Future.delayed(
-//       const Duration(seconds: 2),
-//       () => 'Large Latte',
-//     );
 
 class GrilleDuJeu extends StatefulWidget {
   @override
